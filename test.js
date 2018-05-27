5bbd709899774f9bdebb53b0db78c4c39628b3e7
Leslie Alldridge <Leslie@Leslie-Alldridges-MacBook-Air.local>
2018-05-27 14:05:07 +1200

    debugging files added

---
+++ test.js
@@ -0,0 +1,12 @@
+function addOne (n) {
+  return n + 1
+}
+
+var n = 0
+
+for (var i = 0; i < 10; i++) {
+  n = addOne(n)
+  debugger
+  console.log('The value of `n` is', n)
+  document.write('The value of `n` is ', n, '<br>')
+}
